2882ba0e031b25c599a13e24dffb0eee4de86408
Glessit <glessit.developer@outlook.com>
2017-07-12 21:29:43 +0200

    Add migrations for new entities

---
+++ src/main/java/com/glessit/neurofunky/configuration/ApplicationProfile.java
@@ -0,0 +1,7 @@
+package com.glessit.neurofunky.configuration;
+
+public class ApplicationProfile {
+    public final static String DEVELOPMENT = "DEVELOPMENT";
+    public final static String PRODUCTION = "PRODUCTION";
+    public final static String DEFAULT = "default";
+}
