430945cdcd77e79f440ed4825f7569e8b30ec833
Glessit <glessit.developer@outlook.com>
2017-07-30 15:39:15 +0200

    Fix auto-login after auth.

--- src/main/java/com/glessit/neurofunky/service/ISecurityService.java
+++ src/main/java/com/glessit/neurofunky/service/ISecurityService.java
@@ -4,5 +4,10 @@ package com.glessit.neurofunky.service;
 import com.glessit.neurofunky.service.dto.FacebookToken;
 
 public interface ISecurityService {
+    /**
+     * Do auto-login
+     * @param id fbID for auth
+     * @return
+     */
     FacebookToken login(Long id);
 }
--- src/main/java/com/glessit/neurofunky/service/standard/SecurityService.java
+++ src/main/java/com/glessit/neurofunky/service/standard/SecurityService.java
@@ -1,19 +1,22 @@
 package com.glessit.neurofunky.service.standard;
 
-
-import com.glessit.neurofunky.configuration.security.beans.FacebookAuthentication;
 import com.glessit.neurofunky.configuration.security.beans.FacebookAuthenticationProvider;
+import com.glessit.neurofunky.entity.User;
+import com.glessit.neurofunky.repository.UserRepository;
 import com.glessit.neurofunky.service.ISecurityService;
+import com.glessit.neurofunky.service.ITokenService;
 import com.glessit.neurofunky.service.IUserService;
 import com.glessit.neurofunky.service.dto.FacebookToken;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class SecurityService implements ISecurityService {
+
+    @Autowired
+    private ITokenService tokenService;
 
     // todo: please use cache of security
     /*
@@ -23,16 +26,16 @@ public class SecurityService implements ISecurityService {
     @Autowired
     private IUserService userService;
     @Autowired
+    private UserRepository userRepository;
+    @Autowired
     private FacebookAuthenticationProvider facebookAuthenticationProvider;
 
     @Override
     public FacebookToken login(Long id) {
         log.info("Autologin with id {}", id);
-        Authentication facebookAuthentication = facebookAuthenticationProvider.authenticate(new
-                FacebookAuthentication(String.valueOf(id)));
-        return new FacebookToken(
-                Long.valueOf(facebookAuthentication.getCredentials().toString()),
-                userService.getUserByFacebookId(id)
-        );
+        // generate token
+        User user = userRepository.findOneByFacebookId(id);
+        Long token = tokenService.createTokenForUser(user, tokenService.get());
+        return new FacebookToken(token, user);
     }
 }
